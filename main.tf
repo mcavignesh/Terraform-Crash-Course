740c27780a759657e5b37955d7caa4c1a23dab6e
Vignesh Babu N M <vigneshbabu.nm@acqueon.com>
2021-01-08 22:38:21 +0530

    Updated main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAJTTSSUF2PB6HDCCA"
-  secret_key = "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"
+  #access_key = "AKIAJTTSSUF2PB6HDCCA"
+  #secret_key = "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"
 }
 
 variable "subnet_prefix" {
